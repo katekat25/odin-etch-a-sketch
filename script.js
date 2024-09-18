4b3184a2671b5be9113c3864108c42fcf48025f8
Kate <kate.schumacher@gmail.com>
2024-09-18 17:53:45 -0500

    Change funciton and variable names

--- script.js
+++ script.js
@@ -2,21 +2,21 @@ console.log("Hello worldddd");
 const container = document.querySelector("#container");
 
 let numberOfRows = 5;
-let numberOfDivs = (Math.pow(numberOfRows,2));
+let numberOfPixels = (Math.pow(numberOfRows,2));
 
-makeManyDivs(numberOfDivs);
+drawPixels(numberOfPixels);
 //eventually will take in user input, but not yet
 
 //make a function that creates divs multiple times
 
-function makeManyDivs(numberOfDivs) {
-    for (let i=0; i < numberOfDivs; i++) {
+function drawPixels(numberOfPixels) {
+    for (let i=0; i < numberOfPixels; i++) {
         let newDiv = document.createElement("div");
         newDiv.classList.add("pixel");
         newDiv.textContent = "What up";
         container.appendChild(newDiv);
-        newDiv.style.width = (1000 / Math.sqrt(numberOfDivs)) + "px";
-        newDiv.style.height = (1000 / Math.sqrt(numberOfDivs)) + "px";
+        newDiv.style.width = (1000 / numberOfRows) + "px";
+        newDiv.style.height = (1000 / numberOfRows) + "px";
     }
     
 }
