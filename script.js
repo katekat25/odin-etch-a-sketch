f7429c85b90aae00662c63dd6e40310d8b2b4fd9
Kate <kate.schumacher@gmail.com>
2024-09-19 09:24:18 -0500

    Randomize colors on hover

--- script.js
+++ script.js
@@ -32,13 +32,12 @@ function populateGrid(numberOfPixels, pixelDimensions) {
     }
     for (let i = 0; i < numberOfPixels; i++) {
         let newDiv = document.createElement("div");
-        newDiv.classList.add("pixel");
-        //newDiv.textContent = "I'm a pixel!";
+        newDiv.className = "blank-pixel";
         container.appendChild(newDiv);
         newDiv.style.width = pixelDimensions;
         newDiv.style.height = pixelDimensions;
         newDiv.addEventListener("mouseover", () => {
-            newDiv.classList.add("colored-pixel");
+            newDiv.style.backgroundColor = '#' + Math.floor(Math.random()*16777215).toString(16);
         })
     }
 }
