93e72cab743e531c671908c92c141a43c70274aa
Kate <kate.schumacher@gmail.com>
2024-09-18 22:11:44 -0500

    Prompt to reenter if input is not a whole number

--- script.js
+++ script.js
@@ -6,12 +6,17 @@ inputBox.addEventListener("keypress", function (e) {
     if (e.key === "Enter") {
         let userRows = e.target.value;
         if (userRows > 100) {
-            alert("Please enter a number less than 100.");
+            alert("Please enter a number that's less than 100.");
             e.target.value = '';
             return;
         }
         if (userRows < 1) {
             alert("Please enter a number that's at least 1.");
+            e.target.value = '';
+            return;
+        }
+        if (userRows % 1 != 0) {
+            alert("Please enter a whole number.");
             e.target.value = '';
             return;
         }
